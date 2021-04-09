cee259e5511b08aca298cc481a1079b3b920b7e4
Wesley Gurgel <wesleygurgel27@gmail.com>
2021-04-09 10:19:30 -0300

    Revert "Poupança Ajustes"
    This reverts commit 9f3e3f770d1e27bef922f8a16aea79390362c034.

--- iFome/app/src/main/java/com/example/ifome/MainActivity.java
+++ iFome/app/src/main/java/com/example/ifome/MainActivity.java
@@ -6,10 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,7 +15,6 @@ import com.google.firebase.database.ValueEventListener;
 public class MainActivity extends AppCompatActivity {
 
     private DatabaseReference referencia = FirebaseDatabase.getInstance().getReference();
-    private FirebaseAuth usuario = FirebaseAuth.getInstance();
 
 
     @Override
@@ -27,45 +22,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /* Deslogar usuario */
-        usuario.signOut();
-
-        /* Logar Usuario */
-        usuario.signInWithEmailAndPassword("wesleygurgel27@gmail.com", "wesley123456")
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            Log.i("LoginUser", "Sucesso ao logar o usuário " + usuario.getCurrentUser().getEmail());
-                        }else{
-                            Log.i("LoginUser", "Falha ao logar o usuário" + usuario.getCurrentUser());
-                        }
-                    }
-                });
-
-        /* Verificar se o usuário está logado*/
-
-        if ( usuario.getCurrentUser() != null) {
-            Log.i("Usuario", "Usuario Logado");
-        }else{
-            Log.i("Usuario", "Usuario desLogado");
-
-        }
-
-        /* Cadastro de Usuário
-        usuario.createUserWithEmailAndPassword("wesleygurgel27@gmail.com", "wesley123456")
-                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            Log.i("CreateUser", "Sucesso ao cadastrar o usuário");
-                        }else{
-                            Log.i("CreateUser", "Falha ao cadastrar o usuário");
-                        }
-                    }
-                });*/
-
-        /* DatabaseReference usuarios = referencia.child("usuarios").child("004");
+        DatabaseReference usuarios = referencia.child("usuarios").child("004");
 
         usuarios.addValueEventListener(new ValueEventListener() {
             @Override
@@ -81,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         // Salvar Dados no Firebase
-        DatabaseReference usuarios = referencia.child("usuarios");
+        /* DatabaseReference usuarios = referencia.child("usuarios");
         usuarios.child("003").child("nome").setValue("Wesley");
         referencia.child("usuarios").child("001").child("nome").setValue("Clara");
 
